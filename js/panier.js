d766cbece09cc04aadad0654f042638e2e64fd8a
stephane-maker <contact@mydevproject.fr>
2022-01-18 22:33:27 +0100

    correction de la requete fetch pour l'envoye de la commande du clients a l'api

--- js/panier.js
+++ js/panier.js
@@ -48,17 +48,25 @@ async function CheckCookie() {
                 .catch(function(err) {
                     console.log(err)
                 })
+
         }
         focusMain.appendChild(GeneralClassAccesoire.AffichagePrixTotal(prixTotal));
         //creation du boutton commander
+
+        let divMereBtn = document.createElement("div");
+        divMereBtn.classList = "container_produits"
+        focusMain.appendChild(divMereBtn);
+
         let buttonCommader = document.createElement("a");
 
         buttonCommader.classList = "btn btn-primary btn-lg";
         buttonCommader.textContent = "Commader";
-        focusMain.appendChild(buttonCommader);
+
+        divMereBtn.appendChild(buttonCommader);
         //creation de l'evnt click boutton commander deploiment du formulaire 
         buttonCommader.addEventListener("click", function() {
-            buttonCommader.remove()
+            divMereBtn.remove();
+
             let formForFomulaire = document.createElement("form");
             formForFomulaire.classList = "row g-3";
             focusMain.appendChild(formForFomulaire)
@@ -74,6 +82,7 @@ async function CheckCookie() {
             //creation input email
             let inputEmailUser = document.createElement("input");
             inputEmailUser.setAttribute("type", "email");
+            inputEmailUser.id = "email";
             inputEmailUser.classList = "form-control";
             divContainerEmail.appendChild(inputEmailUser);
 
@@ -114,7 +123,7 @@ async function CheckCookie() {
             //creation label last name User
             let labelForLastNameUser = document.createElement("label");
             labelForLastNameUser.classList = "form-label";
-            labelForLastNameUser.textContent = "First-name";
+            labelForLastNameUser.textContent = "Last-name";
             divMereLastNameUser.appendChild(labelForLastNameUser);
             //cretion input last name User
             let inputForLastNameUser = document.createElement("input");
@@ -122,17 +131,65 @@ async function CheckCookie() {
             inputForLastNameUser.classList = "form-control";
             divMereLastNameUser.appendChild(inputForLastNameUser);
 
+            //Creation  addresse user 
+            let divMereAddresseUser = document.createElement("div");
+            divMereAddresseUser.classList = "col-md-6";
+            formForFomulaire.appendChild(divMereAddresseUser);
+            //creation label last name User
+            let labelForAdresseUser = document.createElement("label");
+            labelForAdresseUser.classList = "form-label";
+            labelForAdresseUser.textContent = "Addresse";
+            divMereAddresseUser.appendChild(labelForAdresseUser);
+            //creation input addresse user 
+            let inputForAddresseUser = document.createElement("input");
+            inputForAddresseUser.setAttribute("type", "text");
+            inputForAddresseUser.classList = "form-control";
+            labelForAdresseUser.appendChild(inputForAddresseUser);
+
             //creation button confirmer votre commande
             let divClassMereButton = document.createElement("div");
             divClassMereButton.classList = "col-12";
             formForFomulaire.appendChild(divClassMereButton);
             //Creation button confirmer la commander
-            let buttonConfirmerCommande = document.createElement("button");
+            let buttonConfirmerCommande = document.createElement("a");
             buttonConfirmerCommande.setAttribute("type", "submit");
             buttonConfirmerCommande.classList = "btn btn-primary";
             buttonConfirmerCommande.textContent = "Commander !";
             divClassMereButton.appendChild(buttonConfirmerCommande);
 
+            buttonConfirmerCommande.addEventListener("click", function() {
+                let verificationEmail = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+                if (inputEmailUser.value.match(verificationEmail) && inputForCityUser.value != "" && inputForFirstNameUser.value != "" && inputForLastNameUser.value != "" && inputForAddresseUser != "") {
+                    EnvoyeDeDonneFetch(inputEmailUser.value, inputForCityUser.value, inputForFirstNameUser.value, inputForLastNameUser.value, inputForAddresseUser.value)
+
+                } else {
+                    console.log("non")
+                }
+            })
         })
+    }
+}
+
+async function EnvoyeDeDonneFetch(email, city, firstName, lastName, address) {
+    let tableauProduit = [];
+    for (let i = 0; i < panier.length; i++) {
+        const element = panier[i];
+        console.log(element["id"]);
+        if (!tableauProduit.hasOwnProperty(element["name"])) {
+            tableauProduit[element["name"]] = [];
+        }
+        tableauProduit[element["name"]].push(element["id"]);
+    }
+    for (let categorie in tableauProduit) {
+        let contact = { "email": email, "city": city, "firstName": firstName, "lastName": lastName, "address": address };
+        let products = tableauProduit[categorie];
+        await fetch("http://localhost:3000/api/" + categorie + "/order", {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ "contact": contact, "products": products })
+        });
     }
 }
